931a685acacd0e793e622da501070674587e32fc
MarcMukesh <35630243+MarcMukesh@users.noreply.github.com>
2018-06-06 17:04:17 +0530

    rasp2 testing

--- rfidreader.py
+++ rfidreader.py
@@ -12,9 +12,7 @@ ser = serial.Serial(last_dev, 19200) # here you have to write your port. If you 
 #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #server_address = ('192.168.137.105', 10005)
 
-print('checking23');
-print('checking24');
-print('checking25');
+print('checking26');
 
 
 
